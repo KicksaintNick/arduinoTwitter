36c083c1f627f8abf06e9c6d465bea5a61b3851e
Nicholas Bester <nick@threefoldcreative.com>
2015-07-20 08:30:37 +0200

    updated the python script to handle tweet overload
    Added case for IncompleteRead
    Updated Version

--- trackTwitter.py
+++ trackTwitter.py
@@ -1,6 +1,7 @@
 # Author: Nicholas Bester
 # Title: Twitter Stream connection
 # Description: Tracks a string using the Twitter API and sends a Serial command to an Arduino once a Tweet matching the tracked string is found
+# Version: 1.1
 
 # imports
 import time
@@ -9,6 +10,8 @@ from TwitterAPI import TwitterAPI
 import struct
 import os
 from serial import Serial
+import httplib
+from httplib import IncompleteRead
 
 # Pretty console colours
 class bcolors:
@@ -22,18 +25,18 @@ class bcolors:
 	UNDERLINE = '\033[4m'
 
 # Variables
-availableArduino = True # Debugging without an Arduino
+availableArduino = False # Debugging without an Arduino
 testSerial = False # Debugging without Twitter connection
-arduinoPort = '[Enter port for Arduino]' # USB port address for the Arduino
+arduinoPort = '[insert here]' # USB port address for the Arduino
 arduinoBaud = '9600' # Baud for serial communication
 arduinoWaitTime = 3 # The length of time Python wait before attemping to issue commands to the Arduino
-stringToTrack = "#nickbester.com" # Change this to the search term you wish to track from Twitter
+stringToTrack = "[insert here]" # Change this to the search term you wish to track from Twitter
 
 # Access requirements for Twitter API connection
-access_token = '[Enter token]'
-access_token_secret = '[Enter secret]'
-consumer_key = '[Enter consumer key]'
-consumer_secret = '[Enter consumer secret]'
+access_token = '[insert here]'
+access_token_secret = '[insert here]'
+consumer_key = '[insert here]'
+consumer_secret = '[insert here]'
 
 # Clearing the screen for aesthetic display of console statements
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -61,29 +64,34 @@ if testSerial:
 	sleep(arduinoWaitTime)
 else:
 	print bcolors.OKGREEN + "Initialisation OK!" + bcolors.ENDC
-
 	print bcolors.WARNING + 'Initialising Twitter Stream API Authorisation' + bcolors.ENDC
+	try:
+		# Setting up a connection to Twitter using the Twitter API
+		api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
 
-	# Setting up a connection to Twitter using the Twitter API
-	api = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+		print bcolors.OKGREEN + "Initialisation OK!" + bcolors.ENDC
+		print bcolors.OKBLUE + 'Twitter Stream Request running' + bcolors.ENDC
 
-	print bcolors.OKGREEN + "Initialisation OK!" + bcolors.ENDC
+		# A request object which opens a stream to Twitter tracking the hashtag in question
+		r = api.request('statuses/filter', {'track':stringToTrack})
 
-	print bcolors.OKBLUE + 'Twitter Stream Request running' + bcolors.ENDC
+		# Checks if text within the stream item is populated and issues a command to the Arduino
+		for item in r.get_iterator():
+			if 'text' in item:
+				print item['user']['screen_name'].encode('utf-8') + ' tweeted: ' + item['text'].encode('utf-8')# Print screen name and the tweet text
 
-	# A request object which opens a stream to Twitter tracking the hashtag in question
-	r = api.request('statuses/filter', {'track':stringToTrack})
-
-	# Checks if text within the stream item is populated and issues a command to the Arduino
-	for item in r.get_iterator():
-		if 'text' in item:
-			print item['user']['screen_name'].encode('utf-8') + ' tweeted: ' + item['text'].encode('utf-8')# Print screen name and the tweet text
-
-			# It is possible to check the tweets for further commands using regular expressions to send multiple commands to the Arduino
-			
-			if availableArduino:
-				print "Arduino turning on the LED"
-				ser.write(bytes(1)) # The command is a simple byte intepretation of the integer 1
-				sleep(arduinoWaitTime) # Wait before sending next command
-				ser.write(bytes(0))
-				sleep(arduinoWaitTime) # Wait before sending next command
+				# It is possible to check the tweets for further commands using regular expressions to send multiple commands to the Arduino
+				
+				if availableArduino:
+					print "Arduino turning on the LED"
+					ser.write(bytes(1)) # The command is a simple byte intepretation of the integer 1
+					sleep(arduinoWaitTime) # Wait before sending next command
+					ser.write(bytes(0))
+					sleep(arduinoWaitTime) # Wait before sending next command
+	except IncompleteRead:
+		# Oh well, reconnect and keep trucking
+		print "IncompleteRead occurred"
+	except KeyboardInterrupt:
+		# Or however you want to exit this loop
+		api.disconnect()
+		exit()
